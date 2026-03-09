2e31862a2566d48322527b3995d5341e4800cf65
SimpzAI <acharyarajesh@gmail.com>
2026-03-09 11:22:58 +0000

    Fix NSE symbol format for TwelveData API

--- simpssmartrade_app/lib/services/market_data_service.dart
+++ simpssmartrade_app/lib/services/market_data_service.dart
@@ -8,8 +8,8 @@ class MarketDataService {
   static Future<double?> getLTP(String symbol) async {
 
     final url = Uri.parse(
-      "https://api.twelvedata.com/price?symbol=$symbol:NSE&apikey=$apiKey"
-    );
+  "https://api.twelvedata.com/price?symbol=$symbol.NSE&apikey=$apiKey"
+  );
 
     final response = await http.get(url);
 
